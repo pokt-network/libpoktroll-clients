838f28865441ca6dd5988aec2121cc47ab8fe29a
Bryan White <bryanchriswhite@gmail.com>
2024-12-05 00:20:07 +0100

    fix: shared library name

--- tests/test_main.c
+++ tests/test_main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "libclients.h"
+#include "libpoktroll_clients.h"
 #include "context.h"
 #include "unity.h"
 #include "poktroll/application/tx.pb-c.h"
